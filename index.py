ffc433724a587799f92f49eedd804539dc404cfb
Zhillan Azmi <jilaann@backboxindonesia.or.id>
2023-12-08 00:50:31 +0700

    mengoneksikan database lalu membuat contoh pemanggilan functionnya

--- index.py
+++ index.py
@@ -1,22 +1,34 @@
 import mysql.connector
 
-db_config = {
-    'host': 'localhost',
-    'user' : 'root',
-    'password' : '',
-    'database' : 'tubespemdas'
-}
+def db_config():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="tubespemdas"
+    )
 
-# Connect to database
-try:
-    connection = mysql.connector.connect(**db_config)
-    if connection.is_connected():
-        print("Database connected")
+# ---------------------------------------------
 
-except Error as e:
-    print(f"Error while connecting to MySQL {e}")
+def create_user(username, password, role, email):
+    connection = db_config()
+    cursor = connection.cursor()
+    query = "INSERT INTO Login (username, password, role, email) VALUES (%s, %s, %s, %s)"
+    cursor.execute(query, (username, password, role, email))
+    connection.commit()
+    connection.close()
 
-finally:
-    if 'connection' in locals() and connection.is_connected():
-        connection.close()
-        print("Database disconnected")
+# Contoh fungsi untuk membaca data
+def read_users():
+    connection = db_config()
+    cursor = connection.cursor()
+    query = "SELECT * FROM Login"
+    cursor.execute(query)
+    for row in cursor:
+        print(row)
+    connection.close()
+
+# Contoh pemanggilan fungsi
+create_user('testuser', 'password123', 'user', 'test@test.com')
+read_users()
+
