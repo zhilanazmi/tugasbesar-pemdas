7b27512257b8371a8b7de1c83af28142d3198cb8
Zhillan Azmi <jilaann@backboxindonesia.or.id>
2023-12-08 00:41:09 +0700

    mengoneksikan database kedalam program python

--- index.py
+++ index.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import mysql.connector
+
+db_config = {
+    'host': 'localhost',
+    'user' : 'root',
+    'password' : '',
+    'database' : 'tubespemdas'
+}
+
+# Connect to database
+try:
+    connection = mysql.connector.connect(**db_config)
+    if connection.is_connected():
+        print("Database connected")
+
+except Error as e:
+    print(f"Error while connecting to MySQL {e}")
+
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Database disconnected")
